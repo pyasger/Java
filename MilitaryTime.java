c819ed3c0ead4da01f4510fe0454ef1c693c865a
Yasger <tw5721uo@go.minnstate.edu>
2021-11-06 13:41:41 -0500

    java

---
+++ MilitaryTime.java
@@ -0,0 +1,12 @@
+public class MilitaryTime {
+	public static void main(String[] args) {
+int time = 12;
+if (time < 10) {
+	System.out.println("Good Morning.");
+}else if (time < 20) {
+	System.out.println("Good day.");
+}else {
+	System.out.println("Good eveNing.");	
+		}
+	}
+}
